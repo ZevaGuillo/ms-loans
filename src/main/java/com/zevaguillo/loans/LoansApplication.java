6a9d8cad38c21af7758750aca619fcfa4303a329
zevaguillo <guiller.zeva16@gmail.com>
2024-10-05 00:25:01 -0500

    update

--- src/main/java/com/zevaguillo/loans/LoansApplication.java
+++ src/main/java/com/zevaguillo/loans/LoansApplication.java
@@ -1,9 +1,25 @@
 package com.zevaguillo.loans;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Guillermo Zevallos",
+						email = "guiller.zeva16@gmail.com"
+				)
+		)
+)
 public class LoansApplication {
 
 	public static void main(String[] args) {
