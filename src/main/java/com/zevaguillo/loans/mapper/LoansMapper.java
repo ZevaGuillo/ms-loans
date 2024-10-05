9422914b07955d4634f91538246951db3996e65b
zevaguillo <guiller.zeva16@gmail.com>
2024-10-05 00:02:04 -0500

    first commit

---
+++ src/main/java/com/zevaguillo/loans/mapper/LoansMapper.java
@@ -0,0 +1,29 @@
+package com.zevaguillo.loans.mapper;
+
+
+import com.zevaguillo.loans.dto.LoansDTO;
+import com.zevaguillo.loans.entity.Loans;
+
+public class LoansMapper {
+
+    public static LoansDTO mapToLoansDto(Loans loans, LoansDTO loansDto) {
+        loansDto.setLoanNumber(loans.getLoanNumber());
+        loansDto.setLoanType(loans.getLoanType());
+        loansDto.setMobileNumber(loans.getMobileNumber());
+        loansDto.setTotalLoan(loans.getTotalLoan());
+        loansDto.setAmountPaid(loans.getAmountPaid());
+        loansDto.setOutstandingAmount(loans.getOutstandingAmount());
+        return loansDto;
+    }
+
+    public static Loans mapToLoans(LoansDTO loansDto, Loans loans) {
+        loans.setLoanNumber(loansDto.getLoanNumber());
+        loans.setLoanType(loansDto.getLoanType());
+        loans.setMobileNumber(loansDto.getMobileNumber());
+        loans.setTotalLoan(loansDto.getTotalLoan());
+        loans.setAmountPaid(loansDto.getAmountPaid());
+        loans.setOutstandingAmount(loansDto.getOutstandingAmount());
+        return loans;
+    }
+
+}
