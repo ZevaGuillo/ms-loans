dcdb3b4ee74c797efa736551d7fdff4cc8e69a76
salvarado <steven.alvarado@sasf.net>
2024-10-09 16:58:49 -0500

    add config server

--- src/main/java/com/zevaguillo/loans/LoansApplication.java
+++ src/main/java/com/zevaguillo/loans/LoansApplication.java
@@ -5,10 +5,14 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.zevaguillo.loans.dto.LoansContactInfoDto;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(LoansContactInfoDto.class)
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Loans microservice REST API Documentation",
